7f6aa06450cefcdd971020c6e3c39ce4b79040fc
Carolina Silva <cboliveirasbr@gmail.com>
2020-10-24 16:38:05 -0300

    Creating Reservation Model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_24_192817) do
+ActiveRecord::Schema.define(version: 2020_10_24_193650) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,18 @@ ActiveRecord::Schema.define(version: 2020_10_24_192817) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id"], name: "index_places_on_user_id"
+  end
+
+  create_table "reservations", force: :cascade do |t|
+    t.date "initial_date"
+    t.date "final_date"
+    t.integer "price"
+    t.bigint "user_id", null: false
+    t.bigint "place_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["place_id"], name: "index_reservations_on_place_id"
+    t.index ["user_id"], name: "index_reservations_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -41,4 +53,6 @@ ActiveRecord::Schema.define(version: 2020_10_24_192817) do
   end
 
   add_foreign_key "places", "users"
+  add_foreign_key "reservations", "places"
+  add_foreign_key "reservations", "users"
 end

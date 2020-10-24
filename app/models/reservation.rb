b55ee414ec993a06304eac496f4bf40960aa67e9
leticiakanegae <lckanegae@gmail.com>
2020-10-24 19:05:50 -0300

    Routes and has_many/has_one references created

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,4 +1,5 @@
 class Reservation < ApplicationRecord
   belongs_to :user
   belongs_to :place
+  has_one :user, through: :places
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :places do
+    resources :reservations, only: [:create]
+  end
+  resources :reservations, only: [:destroy]
+
+  # FALTA ADICIONAR ROTA DO PROFILE
 end

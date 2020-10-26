e6d8c25ead0d09c656d1b396a882a18947fe03e1
Leon Cohen <cohenleonlbc@gmail.com>
2020-10-26 18:23:18 -0300

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :places do
-    resources :reservations, only: [:create, :index]
+    resources :reservations, only: [:create]
   end
   resources :reservations, only: [:destroy]
 

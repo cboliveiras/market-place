f6af0f1017a7b40936f8c48bccf7a2fbd6bff3b9
leticiakanegae <lckanegae@gmail.com>
2020-10-27 22:17:31 -0300

    Non standard routes and controllers created

--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,8 @@ Rails.application.routes.draw do
   resources :reservations, only: [:destroy]
 
   # FALTA ADICIONAR ROTA DO PROFILE
+  get 'my_places', to: 'pages#test'
+  # places#myplaces
+  get 'my_reservations', to: 'pages#test'
+  # reservations#myreservations
 end

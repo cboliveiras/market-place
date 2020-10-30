341d11487f43897221952b0dec6a6deac53588fe
Gilberto Santos <giba.dosantos@gmail.com>
2020-10-29 21:03:10 -0300

    adding users and places db:seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,23 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+5.times do
+    User.create(
+        first_name: Faker::Name.first_name,
+        last_name: Faker::Name.last_name,
+        email: Faker::Internet.email,
+        password: "lewagon"
+    )    
+end
+
+users = User.all
+
+location_types = ["house", "apartment"]
+users.each do |user|
+    10.times do
+        Place.create(
+            location: Faker::Games::Pokemon.location,
+            price_per_day: rand(50..100),
+            name: Faker::Games::SuperSmashBros.stage,
+            location_type: location_types.sample, 
+            user_id: user.id
+        )
+    end
+end

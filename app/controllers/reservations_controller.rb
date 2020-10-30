54607755e5cf3224ea820b3845238dbe3612bbe2
Leon Cohen <cohenleonlbc@gmail.com>
2020-10-29 23:23:03 -0300

    Added reservation creation in 'show' action when logged as User

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,2 +1,29 @@
 class ReservationsController < ApplicationController
+  def create
+    @reservation = Reservation.new(reservation_params)
+    @reservation.user_id = current_user.id
+    @place = Place.find(params[:place_id])
+    @reservation.place = @place
+    calculate_price
+
+    if @reservation.save
+      redirect_to root_path
+      # Substitute by my_reservations_path after creating it.
+    else
+      redirect_to new_user_session_path, notice: 'You are not logged in.'
+    end
+  end
+
+  private
+
+  def calculate_price
+    days = @reservation.final_date - @reservation.initial_date
+    price = @place.price_per_day
+    total_price = price * days
+    @reservation.price = total_price
+  end
+
+  def reservation_params
+    params.require(:reservation).permit(:initial_date, :final_date)
+  end
 end
--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,5 +1,4 @@
 class Reservation < ApplicationRecord
   belongs_to :user
   belongs_to :place
-  has_one :user, through: :places
 end

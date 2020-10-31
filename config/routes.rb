406804502c020904edf6b04a5ecf49ec56320569
leticiakanegae <lckanegae@gmail.com>
2020-10-31 19:17:43 -0300

    Routes created

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :places do
     resources :reservations, only: [:create]
+    resources :reviews, only: [:new, :create]
   end
   resources :reservations, only: [:destroy]
 

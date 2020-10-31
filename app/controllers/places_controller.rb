d694d4a898caf2e088f05b0713ffa1e55239f440
Carolina Silva <cboliveirasbr@gmail.com>
2020-10-30 22:03:41 -0300

    Add a new place as a placeowner

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,5 +1,25 @@
 class PlacesController < ApplicationController
+  skip_before_action :authenticate_user!, only: [:home, :show]
+
+  def index
+    @places = Place.all
+  end
+
   def show
     @place = Place.find(params[:id])
+  end
+
+  def new
+    @place = Place.new
+  end
+
+  def create
+    @place = Place.new(place_params)
+    @place.user = current_user
+    if @place.save
+      redirect_to place_path(@place), notice: 'Place created!'
+    else
+      render :new
+    end
   end
 end

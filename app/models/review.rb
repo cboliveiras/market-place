806f1208b4b94cc4a0114389f3a503f0ee9926da
leticiakanegae <lckanegae@gmail.com>
2020-10-31 19:02:13 -0300

    Validations added(2)

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,6 +1,7 @@
 class Review < ApplicationRecord
   belongs_to :user
   belongs_to :place
+  has_one :user, through: :places
 
   validates :user_rating, inclusion: { in: 0..5 }
   validates :place_rating, inclusion: { in: 0..5 }

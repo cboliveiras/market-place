313966488c8e0a7c20315ccadddb7bd4d9fbf94b
leticiakanegae <lckanegae@gmail.com>
2020-11-03 23:59:56 -0300

    Date validation begin < end

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -6,12 +6,14 @@ class ReservationsController < ApplicationController
     @place = Place.find(params[:place_id])
     @reservation.place = @place
     calculate_price
-    if @reservation.initial_date.past? || @reservation.final_date.past?
+    if @reservation.initial_date.past? || @reservation.final_date.past? || @reservation.final_date < @reservation.initial_date
       redirect_to place_path(@place), notice: 'Please choose a valid date.'
-    # elsif @reservation.
+    # elsif booked?(@place)
+      # redirect_to place_path(@place), notice: 'Place already booked for those dates.'
+    # CHECAR SE A DATA JA FOI BOOKADA
+    # elsif @place.reservations.include?
       # @place.reservations include
-      # WHERE   where("start_date < ? AND end_date > ?", start_date, end_date)
-      # CHECAR SE A DATA JA FOI BOOKADA -
+      # WHERE   where("initial_date < ? AND final_date > ?", initial_date, final_date)
       # redirect_to place_path(@place), notice: 'Place already booked for those dates.'
     else
       if @reservation.save
@@ -43,4 +45,12 @@ class ReservationsController < ApplicationController
   def reservation_params
     params.require(:reservation).permit(:initial_date, :final_date)
   end
+
+  # def booked?(place)
+  #   new_reservation = @reservation.initial_date..@reservation.final_date
+  #   place.reservations.each do |reservation|
+  #     range_booked = reservation.initial_date..reservation.final_date
+  #     range_booked.cover?(new_reservation.begin) && range_booked.cover?(new_reservation.end)
+  #   end
+  # end
 end

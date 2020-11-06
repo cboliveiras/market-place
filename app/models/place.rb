75545388990f073a76974a8480ccb181d140a8df
leticiakanegae <lckanegae@gmail.com>
2020-11-06 12:21:25 -0300

    Presence validations added

--- app/models/place.rb
+++ app/models/place.rb
@@ -3,6 +3,11 @@ class Place < ApplicationRecord
 
   has_many :reservations
   has_many :reviews
-  
+
   has_one_attached :image
+
+  validates :name, presence: true
+  validates :location, presence: true
+  validates :location_type, presence: true
+  validates :price_per_day, presence: true
 end
